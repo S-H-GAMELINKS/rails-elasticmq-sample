fbfb414941face538c7761a502bbb97e84dc0138
S-H-GAMELINKS <gamelinks007@gmail.com>
2023-06-09 22:33:32 +0900

    Add setting for using SQS

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module RailsElasticmqSample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Using SQS for ActiveJob
+    config.active_job.queue_adapter = :amazon_sqs
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
